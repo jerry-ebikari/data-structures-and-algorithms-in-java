de2b7b883b3b5c0f84c558adf07b76af226d2f77
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-01-07 23:00:22 +0100

    Implemented bubble sort

---
+++ bin/com/algorithms/sort/README.md
@@ -0,0 +1,5 @@
+# Bubble Sort
+
+Bubble sort is a sorting algorithm that repeatedly goes through a list, comparing pairs of adjacent elements, and swaps them if they are in the wrong order. If the list is traversed without the need to swap elements, then the list is sorted and all checks stop.
+
+![Visualization](../../../../assets/images/algorithms/sort/bubble-sort.gif)
---
+++ src/com/algorithms/sort/BubbleSort.java
@@ -0,0 +1,50 @@
+package com.algorithms.sort;
+
+// Algorithm is type generic
+// Doesn't work for native types like int, double
+// Works with types like Integer, Double, String...
+
+public class BubbleSort<T extends Comparable<T>> {
+    // Equivalent int sorting algorithm header would be:
+      // public static int[] sort(int[] arr)
+    public static <T extends Comparable<T>> T[] sort(T[] arr) {
+        boolean swapped = true;
+        int length = arr.length;
+        while (swapped) {
+            swapped = false;
+            for (int i = 1; i < length; i++) {
+                if (arr[i].compareTo(arr[i - 1]) < 0) {
+                    swap(i, i - 1, arr);
+                    swapped = true;
+                }
+            }
+            length--;
+        }
+        return arr;
+    }
+    
+    // SWAP ARRAY VALUES
+    private static <T extends Comparable<T>> void swap(int index1, int index2, T[] arr) {
+        T temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+
+    // TEST
+    public static void main(String[] args) {
+        String[] words = {"Hi", "Hey", "Abs"};
+        Integer[] numbers = {4, -2, 2, 1};
+        sort(words);
+        sort(numbers);
+
+        System.out.println("Sorted Words");
+        for (String word: words) {
+            System.out.println(word);
+        }
+
+        System.out.println("Sorted Numbers");
+        for (int number: numbers) {
+            System.out.println(number);
+        }
+    }
+}
