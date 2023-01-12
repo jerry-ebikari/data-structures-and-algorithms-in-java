704a016c6202892a94ebd59f79c8bacd6a329a50
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-01-12 04:56:03 +0100

    Implemented Sum1ToN

---
+++ src/com/algorithms/Sum1ToN.java
@@ -0,0 +1,13 @@
+package com.algorithms;
+
+public class Sum1ToN {
+    public static int sum(int n) {
+        return n * (n + 1) / 2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(3));
+        System.out.println(sum(5));
+        System.out.println(sum(10));
+    }
+}
