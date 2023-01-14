822ee064f9ed8ccd90a47446f62a0f84a8d1b606
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-01-14 21:08:29 +0100

    Implemented SimpleAdding iteratively

--- src/com/coderbytes/simpleAdding/SimpleAdding.java
+++ src/com/coderbytes/simpleAdding/SimpleAdding.java
@@ -4,9 +4,17 @@ public class SimpleAdding {
     public static long sumUpToN(long n) {
         return n * (n + 1) / 2;
     }
+
+    public static long iterativeSumUpToN(long n) {
+        long sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
     
     public static void main(String[] args) {
-        System.out.println(sumUpToN(12));
-        System.out.println(sumUpToN(140));
+        System.out.println(iterativeSumUpToN(12));
+        System.out.println(iterativeSumUpToN(140));
     }
 }
