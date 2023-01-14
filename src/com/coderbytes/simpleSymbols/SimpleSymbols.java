53b96bf10a3b43f55f1f9e6d9c836eb916cb3163
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-01-14 22:23:31 +0100

    Implemented SimpleSymbols iteratively

--- src/com/coderbytes/simpleSymbols/SimpleSymbols.java
+++ src/com/coderbytes/simpleSymbols/SimpleSymbols.java
@@ -9,6 +9,25 @@ public class SimpleSymbols {
         return str.split(regexString).length > 1 ? "false" : "true";
     }
 
+    public static String iterativeSimpleSymbols(String str) {
+        int length = str.length();
+        char[] charArray = str.toCharArray();
+        // return false if either the first or the last character is a letter
+        if (Character.isLetter(charArray[0]) || Character.isLetter(charArray[length - 1])) {
+            return "false";
+        }
+        for (int i = 1; i < length - 1; i++) {
+            if (Character.isLetter(charArray[i])) {
+                if (charArray[i - 1] == '+' && charArray[i + 1] == '+') {
+                    i++;
+                    continue;
+                }
+                return "false";
+            }
+        }
+        return "true";
+    }
+
     public static void main(String[] args) {
         System.out.println(simpleSymbols("+d+=3=+s+"));
         System.out.println(simpleSymbols("f++d+"));
