43a66de476c60e1c97a3bae7dc188b6c86936fd9
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-01-14 21:25:49 +0100

    Implemented SimpleAdding recursively

--- src/com/coderbytes/simpleAdding/SimpleAdding.java
+++ src/com/coderbytes/simpleAdding/SimpleAdding.java
@@ -2,19 +2,26 @@ package com.coderbytes.simpleAdding;
 
 public class SimpleAdding {
     public static long sumUpToN(long n) {
+        if (n < 1) return n;
         return n * (n + 1) / 2;
     }
 
     public static long iterativeSumUpToN(long n) {
+        if (n < 1) return n;
         long sum = 0;
-        for (int i = 0; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             sum += i;
         }
         return sum;
     }
+
+    public static long recursiveSumUpToN(long n) {
+        if (n < 2) return n;
+        return n + recursiveSumUpToN(n - 1);
+    }
     
     public static void main(String[] args) {
-        System.out.println(iterativeSumUpToN(12));
-        System.out.println(iterativeSumUpToN(140));
+        System.out.println(sumUpToN(12));
+        System.out.println(sumUpToN(140));
     }
 }
