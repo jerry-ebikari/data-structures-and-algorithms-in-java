f15f08e578a9c9400424afad8a2954266d7a4999
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-01-14 22:28:47 +0100

    Fixed SimpleAdding

--- src/com/coderbytes/simpleSymbols/SimpleSymbols.java
+++ src/com/coderbytes/simpleSymbols/SimpleSymbols.java
@@ -4,7 +4,7 @@ public class SimpleSymbols {
     public static String simpleSymbols(String str) {
         String letterAtStart = "^[a-zA-Z]";
         String letterAtEnd = "[a-zA-Z]$";
-        String letterNotSurroundedByPlus = "[^+][a-zA-Z][^+]";
+        String letterNotSurroundedByPlus = "[^+][a-zA-Z]|[a-zA-Z][^+]";
         String regexString = letterAtStart + "|" + letterAtEnd + "|" + letterNotSurroundedByPlus;
         return str.split(regexString).length > 1 ? "false" : "true";
     }
