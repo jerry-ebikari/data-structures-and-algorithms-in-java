161c4e97e9884363a3a82f9a05cb985027be393e
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-01-14 16:35:59 +0100

    Reverse string with char array

--- src/com/coderbytes/firstReverse/FirstReverse.java
+++ src/com/coderbytes/firstReverse/FirstReverse.java
@@ -6,6 +6,18 @@ public class FirstReverse {
         return sb.reverse().toString();
     }
 
+    public static String reverseWithArray(String str) {
+        char[] arr = str.toCharArray();
+        int j = arr.length - 1;
+        for (int i = 0; i < j; i++) {
+            char temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+        return String.valueOf(arr);
+    }
+
     public static void main(String[] args) {
         System.out.println(reverse("coderbyte"));
         System.out.println(reverse("I Love Code"));
