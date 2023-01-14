bf8ed58edd860a3e633bacc8a186fbeb0b763f22
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-01-14 20:54:44 +0100

    Updated LetterChanges

--- src/com/coderbytes/letterChanges/LetterChanges.java
+++ src/com/coderbytes/letterChanges/LetterChanges.java
@@ -5,30 +5,24 @@ public class LetterChanges {
         StringBuilder sb = new StringBuilder(str.length());
         str = str.toLowerCase();
         char[] charArray = str.toCharArray();
+        String vowels = "aeiou";
         for (char character: charArray) {
             if (Character.isLetter(character)) {
                 if (character != 'z') {
-                    sb.append((char)(character + 1));
+                    char nextCharacter = (char)(character + 1);
+                    if (vowels.contains(nextCharacter + "")) {
+                        char c = Character.toUpperCase(nextCharacter);
+                        sb.append(c);
+                    }
+                    else {
+                        sb.append(nextCharacter);
+                    }
                 }
                 else {
-                    sb.append('a');
+                    sb.append('A');
                 }
             }
-            else {
-                sb.append(character);
-            }
-        }
-        return makeVowelsUppercase(sb.toString());
-    }
-
-    public static String makeVowelsUppercase(String str) {
-        char[] charArray = str.toCharArray();
-        String vowels = "aeiou";
-        StringBuilder sb = new StringBuilder(str.length());
-        for (char character: charArray) {
-            if (vowels.contains(character + "")) {
-                sb.append(Character.toUpperCase(character));
-            }
+            // if character is not a letter
             else {
                 sb.append(character);
             }
@@ -39,5 +33,6 @@ public class LetterChanges {
     public static void main(String[] args) {
       System.out.println(letterChanges("hello*3"));
       System.out.println(letterChanges("fun times!"));
+      System.out.println(letterChanges("abcdefghijklmnopqrstuvwxyz"));
     }
 }
