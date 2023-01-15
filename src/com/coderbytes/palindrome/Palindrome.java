7152ac4c1a207632108b0fbc85084fae777c1c8a
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-01-15 17:06:18 +0100

    Implemented iterative Palindrome

--- src/com/coderbytes/palindrome/Palindrome.java
+++ src/com/coderbytes/palindrome/Palindrome.java
@@ -6,6 +6,19 @@ public class Palindrome {
         return str1.equals(new StringBuilder(str1).reverse().toString()) ? "true" : "false";
     }
 
+    public static String iterativePalindrome(String str) {
+        String str1 = str.replaceAll(" ", "");
+        int strLength = str1.length();
+        int j = strLength - 1;
+        for (int i = 0; i < strLength; i++) {
+            if (str1.charAt(i) != str1.charAt(j)) {
+                return "false";
+            }
+            j--;
+        }
+        return "true";
+    }
+
     public static void main(String[] args) {
         System.out.println(palindrome("never odd or even"));
         System.out.println(palindrome("eye"));
