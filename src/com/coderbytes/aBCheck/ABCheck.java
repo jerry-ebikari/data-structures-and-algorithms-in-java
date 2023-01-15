143e74df5940cf99b1a66b8b676cdd2600223fb3
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-01-15 04:40:42 +0100

    Fixed ABCheck

--- src/com/coderbytes/aBCheck/ABCheck.java
+++ src/com/coderbytes/aBCheck/ABCheck.java
@@ -2,7 +2,7 @@ package com.coderbytes.aBCheck;
 
 public class ABCheck {
     public static String aBCheck(String str) {
-        return str.split("a.{3}b").length > 1 ? "true" : "false";
+        return str.split("a.{3}b|b.{3}a").length > 1 ? "true" : "false";
     }
 
     public static void main(String[] args) {
