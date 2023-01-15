37454af446ec2b1f676f94661ab72a9857963796
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-01-15 16:37:08 +0100

    Fixed ExOh

--- src/com/coderbytes/exOh/ExOh.java
+++ src/com/coderbytes/exOh/ExOh.java
@@ -2,6 +2,7 @@ package com.coderbytes.exOh;
 
 public class ExOh {
     public static String exOh(String str) {
+        if (str.length() < 2) return "false";
         int strLength = str.length();
         int numberOfXes = strLength - str.replaceAll("[xX]", "").length();
         int numberOfOs = strLength - str.replaceAll("[oO]", "").length();
@@ -9,6 +10,7 @@ public class ExOh {
     }
 
     public static String exOh2(String str) {
+        if (str.length() < 2) return "false";
         int numberOfOs = 0;
         int strLength = str.length();
         for (int i = 0; i < strLength; i++) {
