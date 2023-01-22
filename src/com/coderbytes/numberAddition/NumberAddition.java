5dcc8a03582646c6fea68113d06c95335fac7339
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-01-22 17:01:49 +0100

    Fixed NumberFormatException in Number Addition

--- src/com/coderbytes/numberAddition/NumberAddition.java
+++ src/com/coderbytes/numberAddition/NumberAddition.java
@@ -5,7 +5,10 @@ public class NumberAddition {
         String[] arr = str.split("[^0-9]+");
         int sum = 0;
         for (String i: arr) {
-            sum += Integer.parseInt(i);
+            try {
+                sum += Integer.parseInt(i);
+            }
+            catch (NumberFormatException e) {}
         }
         return sum;
     }
@@ -13,5 +16,6 @@ public class NumberAddition {
     public static void main(String[] args) {
         System.out.println(numberAddition("75Number9"));
         System.out.println(numberAddition("10 2One Number*1*"));
+        System.out.println(numberAddition(""));
     }
 }
