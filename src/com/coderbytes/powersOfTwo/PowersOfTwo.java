4c2c897f251f53b9bac0ef813e80466248c40e87
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-01-23 13:14:19 +0100

    Implemented Powers of Two

--- src/com/coderbytes/powersOfTwo/PowersOfTwo.java
+++ src/com/coderbytes/powersOfTwo/PowersOfTwo.java
@@ -1,5 +1,20 @@
 package com.coderbytes.powersOfTwo;
 
 public class PowersOfTwo {
-    
+    public static String powersOfTwo(int num) {
+        while (num > 1) {
+            if (num % 2 != 0) {
+                return "false";
+            }
+            num /= 2;
+        }
+        return "true";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(powersOfTwo(4));
+        System.out.println(powersOfTwo(124));
+        System.out.println(powersOfTwo(22));
+        System.out.println(powersOfTwo(16));
+    }
 }
