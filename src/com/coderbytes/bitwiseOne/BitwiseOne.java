b2abbd3f01caa1186f785086ee6cdf1af0044a68
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-01-28 08:29:45 +0100

    Changed implementation for Bitwise One

--- src/com/coderbytes/bitwiseOne/BitwiseOne.java
+++ src/com/coderbytes/bitwiseOne/BitwiseOne.java
@@ -5,11 +5,11 @@ public class BitwiseOne {
         int length = strArr[0].length();
         StringBuilder sb = new StringBuilder(length);
         for (int i = 0; i < length; i++) {
-            if (strArr[0].charAt(i) == '0' && strArr[1].charAt(i) == '0') {
-                sb.append("0");
+            if (strArr[0].charAt(i) == '1' || strArr[1].charAt(i) == '1') {
+                sb.append("1");
                 continue;
             }
-            sb.append("1");
+            sb.append("0");
         }
         return new String(sb);
     }
