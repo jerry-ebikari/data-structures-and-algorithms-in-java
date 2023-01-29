f2f7c97e2b026ba97d9d6ef8a604a6f8be83708c
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-01-29 08:36:03 +0100

    Reimplemented Binary Reversal using built-in methods

--- src/com/coderbytes/binaryReversal/BinaryReversal.java
+++ src/com/coderbytes/binaryReversal/BinaryReversal.java
@@ -2,34 +2,19 @@ package com.coderbytes.binaryReversal;
 
 public class BinaryReversal {
     public static String binaryReversal(String str) {
-        String binary = decimalToBinary(Integer.parseInt(str));
-        binary = new StringBuilder(binary).reverse().toString();
-        return binaryToDecimal(binary) + "";
-    }
-
-    public static String decimalToBinary(int n) {
-        StringBuilder sb = new StringBuilder();
-        while (n > 0) {
-            sb.append(n % 2);
-            n /= 2;
-        }
-        int padding = sb.length() % 8;
+        int decimalNumber = Integer.parseInt(str);
+        String binaryString = Integer.toString(decimalNumber, 2);
+        int binaryStringLength = binaryString.length();
+        // pad binary string if necessary
+        int padding = binaryStringLength % 8;
         if (padding != 0) {
-            padding = 8 - padding;
-            for (int i = 0; i < padding; i++) {
-                sb.append("0");
-            }
+            binaryString = "0".repeat(8 - padding) + binaryString;
         }
-        return new String(sb.reverse());
-    }
-
-    public static int binaryToDecimal(String str) {
-        int length = str.length();
-        int decimal = 0;
-        for (int i = 0; i < length; i++) {
-            decimal += Integer.parseInt(str.charAt(i) + "") * Math.pow(2, length - i - 1);
-        }
-        return decimal;
+        // reverse the binary string
+        StringBuilder sb = new StringBuilder(binaryString).reverse();
+        // convert from binary to decimal
+        decimalNumber = Integer.parseInt(sb.toString(), 2);
+        return decimalNumber + "";
     }
 
     public static void main(String[] args) {
