9e01b9204231333920ae7029167dbf8cc5932073
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-01-31 20:48:01 +0100

    Implemented Two Sum

---
+++ src/com/coderbytes/twoSum/TwoSum.java
@@ -0,0 +1,33 @@
+package com.coderbytes.twoSum;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TwoSum {
+    public static String twoSum(int[] arr) {
+        int target = arr[0];
+        List<String> twos = new ArrayList<>();
+        for (int i = 1; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == target) {
+                    twos.add(arr[i] + "," + arr[j]);
+                }
+            }
+        }
+        if (twos.size() == 0) return "-1";
+        StringBuilder sb = new StringBuilder();
+        for (String elem: twos) {
+            sb.append(elem + " ");
+        }
+        return new String(sb);
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {17, 4, 5, 6, 10, 11, 4, -3, -5, 3, 15, 2, 7};
+        int[] arr2 = {7, 6, 4, 1, 7, -2, 3, 12};
+        int[] arr3 = {7, 3, 5, 2, -4, 8, 11};
+        System.out.println(twoSum(arr1));
+        System.out.println(twoSum(arr2));
+        System.out.println(twoSum(arr3));
+    }
+}
