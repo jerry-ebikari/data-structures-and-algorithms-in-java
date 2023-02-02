a3b172edc98a35a526164727c5814ea04310857a
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-02-02 21:57:45 +0100

    Fixed Product Digits

--- src/com/coderbytes/productDigits/ProductDigits.java
+++ src/com/coderbytes/productDigits/ProductDigits.java
@@ -7,14 +7,14 @@ public class ProductDigits {
     public static int productDigits(int num) {
         List<Integer> quotients = getQuotients(num);
         int length = quotients.size();
-        int maxDigits = 0;
+        int minDigits = Integer.toString(num).length() + 1;
         for (int i = 0, j = length - 1; i <= j; i++, j--) {
             int productLength = (quotients.get(i) + "" + quotients.get(j)).length();
-            if (productLength > maxDigits) {
-                maxDigits = productLength;
+            if (productLength < minDigits) {
+                minDigits = productLength;
             }
         }
-        return maxDigits;
+        return minDigits;
     }
 
     public static List<Integer> getQuotients(int num) {
