9e2ba863e7beb0aa09f54cee578b7ca81c39a2b2
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-03-24 17:52:42 +0100

    Implemented method checkRepeated

--- src/com/algorithms/checkRepeated/CheckRepeated.java
+++ src/com/algorithms/checkRepeated/CheckRepeated.java
@@ -3,7 +3,7 @@ package com.algorithms.checkRepeated;
 public class CheckRepeated {
 
     public static boolean checkRepeated(String str) {
-        
+        return str.matches("(.)(?=\\1)");
     }
 
     public static void main(String[] args) {
