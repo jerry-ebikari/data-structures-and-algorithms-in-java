65a98add5049fb23a8dce86ac65470d162bd47de
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-03-24 18:10:13 +0100

    Implemented method numberTriplet

--- src/com/algorithms/numberTriplet/NumberTriplet.java
+++ src/com/algorithms/numberTriplet/NumberTriplet.java
@@ -3,7 +3,7 @@ package com.algorithms.numberTriplet;
 public class NumberTriplet {
 
     static boolean numberTriplet(String str) {
-        
+        return str.matches(".*(\\d)+(?=(\\s\\1\\s){2}).*");
     }
 
     public static void main(String[] args) {
