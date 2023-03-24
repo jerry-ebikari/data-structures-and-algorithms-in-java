9de2e9c362b24dcbbeba78a4f9b357c3b53b39ba
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-03-24 17:50:10 +0100

    Created method checkRepeated

--- src/com/algorithms/checkRepeated/CheckRepeated.java
+++ src/com/algorithms/checkRepeated/CheckRepeated.java
@@ -1,6 +1,11 @@
 package com.algorithms.checkRepeated;
 
 public class CheckRepeated {
+
+    public static boolean checkRepeated(String str) {
+        
+    }
+
     public static void main(String[] args) {
         
     }
