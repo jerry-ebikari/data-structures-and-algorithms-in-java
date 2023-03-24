d3cdeb7413d43197352ebfca239fb6c9a9f20807
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-03-24 18:07:52 +0100

    Created method numberTriplet

--- src/com/algorithms/numberTriplet/NumberTriplet.java
+++ src/com/algorithms/numberTriplet/NumberTriplet.java
@@ -1,6 +1,11 @@
 package com.algorithms.numberTriplet;
 
 public class NumberTriplet {
+
+    static boolean numberTriplet(String str) {
+        
+    }
+
     public static void main(String[] args) {
         
     }
