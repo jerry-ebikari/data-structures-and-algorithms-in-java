b36bde93514a86b57570ba50168c901a3c215762
jerry-ebikari <jerryebikarineedam@gmail.com>
2023-03-27 21:26:13 +0100

    Fix NumberTriplet

--- src/com/algorithms/numberTriplet/NumberTriplet.java
+++ src/com/algorithms/numberTriplet/NumberTriplet.java
@@ -3,10 +3,10 @@ package com.algorithms.numberTriplet;
 public class NumberTriplet {
 
     static boolean numberTriplet(String str) {
-        return str.matches(".*(\\d)+(?=(\\s\\1\\s){2}).*");
+        return str.matches(".*(\\d+)(?=.*(?:\\s\\1).*).*");
     }
 
     public static void main(String[] args) {
-        
+        System.out.println(numberTriplet("10 men went on 10 dates with 10 women"));
     }
 }
